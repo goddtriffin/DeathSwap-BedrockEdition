342b26ead83befc472aba5542be89e163deb7031
Todd Everett Griffin <goddtriffin@gmail.com>
2020-11-03 23:21:16 -0600

    Remove unnecessary filetypes from webpack config.

--- BedrockEdition/webpack.config.js
+++ BedrockEdition/webpack.config.js
@@ -10,14 +10,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
+                test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
         ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['.ts'],
     },
     output: {
         path: path.resolve(__dirname, `DeathSwapBehaviourPack/scripts`),
