b6709e30d9d7887e18b991b33ea59abafb44bfbe
Todd Everett Griffin <goddtriffin@gmail.com>
2020-11-06 20:19:22 -0600

    Add TS docs for system bindings.

--- BedrockEdition/src/minecraft-bedrock-edition/client.ts
+++ BedrockEdition/src/minecraft-bedrock-edition/client.ts
@@ -4,8 +4,15 @@ export interface Client {
     registerSystem(majorVersion: number, minorVersion: number): System;
 
     /**
-     * Logging Bindings
+     * LOGGING BINDINGS
+     * ================
      */
 
+     /**
+     * log allows for logging a message to the ContentLog file.
+     * On Windows 10 devices it is located at ' %APPDATA%\..\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\logs '.
+     * 
+     * @param {string} message - The message that you want to send to the log file.
+     */
     log(message: string): void;
 }
